5422cd403dc074fdbc31980da586d16b5598c7d5
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-15 01:44:50 +0300

    Finish assignment

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-
 import NavBar from "../components/NavBar";
 
 test("renders a <nav> element", () => {
